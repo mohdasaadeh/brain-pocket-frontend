e8f7719926df2318ba4250a706658106eb4aed39
mohdasaadeh <mass.1993@hotmail.com>
2022-06-08 21:00:54 +0300

    Adding form validation

--- src/components/forms/ListForm/index.js
+++ src/components/forms/ListForm/index.js
@@ -1,4 +1,3 @@
-import { useNavigate } from "react-router-dom";
 import { Form, Field } from "react-final-form";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
@@ -7,7 +6,21 @@ import Grid from "@mui/material/Grid";
 import ListFormStateToRedux from "./ListFormStateToRedux";
 
 const ListForm = ({ onSubmit, onCancel, initialValues }) => {
-  const navigate = useNavigate();
+  const required = (value) => (value ? undefined : "Required");
+
+  const minValue = (min) => (value) =>
+    value.length >= min ? undefined : `Should be greater than ${min}`;
+
+  const maxValue = (max) => (value) =>
+    value.length <= max ? undefined : `Should be less than ${max}`;
+
+  const composeValidators =
+    (...validators) =>
+    (value) =>
+      validators.reduce(
+        (error, validator) => error || validator(value),
+        undefined
+      );
 
   const renderField = ({ input, meta, placeholder }) => {
     return (
@@ -16,6 +29,8 @@ const ListForm = ({ onSubmit, onCancel, initialValues }) => {
         label={placeholder}
         variant="standard"
         style={{ minWidth: 400 }}
+        error={meta.error && meta.touched ? true : false}
+        helperText={meta.error && meta.touched && meta.error}
       />
     );
   };
@@ -36,6 +51,11 @@ const ListForm = ({ onSubmit, onCancel, initialValues }) => {
                   name="title"
                   component={renderField}
                   placeholder="List Title"
+                  validate={composeValidators(
+                    required,
+                    minValue(3),
+                    maxValue(30)
+                  )}
                 />
               </div>
               <div style={{ marginTop: 10 }}>
@@ -43,6 +63,11 @@ const ListForm = ({ onSubmit, onCancel, initialValues }) => {
                   name="firstColumnTitle"
                   component={renderField}
                   placeholder="Card Front Title"
+                  validate={composeValidators(
+                    required,
+                    minValue(3),
+                    maxValue(30)
+                  )}
                 />
               </div>
               <div style={{ marginTop: 10 }}>
@@ -50,6 +75,11 @@ const ListForm = ({ onSubmit, onCancel, initialValues }) => {
                   name="secondColumnTitle"
                   component={renderField}
                   placeholder="Card Back Title"
+                  validate={composeValidators(
+                    required,
+                    minValue(3),
+                    maxValue(30)
+                  )}
                 />
               </div>
               <div className="buttons" style={{ marginTop: 10 }}>
